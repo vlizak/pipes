38d209dea4be724428e972a564dde8d60bee5051
vlizak <viktor.lizak@ericsson.com>
2018-01-18 15:04:31 +0100

    desired behaviour

--- pipe.cpp
+++ pipe.cpp
@@ -39,7 +39,7 @@ int main(int argc, char *argv[])
         printf("An error occured while connecting read end of the pipe.\n");
     }
 //    fcntl(pipefd[1], F_SETFL, O_NONBLOCK);
-//    fcntl(pipefd[0], F_SETFL, O_NONBLOCK);
+    fcntl(pipefd[0], F_SETFL, O_NONBLOCK);
 
     ev.events = EPOLLOUT | EPOLLET;
     ev.data.fd = pipefd[1];
@@ -53,33 +53,43 @@ int main(int argc, char *argv[])
     printf("fd of read end of the pipe = %d\n", pipefd[0]);
     printf("fd of write end of the pipe = %d\n", pipefd[1]);
 
-    printf("sizeof toPrint = %lu\n", sizeof(toPrint));
-    while (1)
+    bool readEndClosed = false, writeEndClosed = false;
+
+    while (!readEndClosed && !writeEndClosed)
     {
         printf("In while\n");
         nfds = epoll_wait(epollfd, events, MAX_EVENTS, -1);    
         printf("nfds = %d\n", nfds);
+        int messageSize;
         
         for (int i = 0; i < nfds; ++i)        
         {
             if (events[i].data.fd == pipefd[0])
             {
                 printf("Selected fd = %d\n", events[i].data.fd);
-                while (read(pipefd[0], &buf, 1) > 0)
+                while ( (messageSize = read(pipefd[0], &buf, 1)) > 0)
                 {
                     write(STDOUT_FILENO, &buf, 1);
+                    printf("Byte written, messageSize = %d\n", messageSize);
                 } 
-                printf("Closing write end side\n");
-                close(pipefd[1]);
+                printf("After readend while\n");
+                if (0 == messageSize)
+                {
+                    printf("No more messages to read.\n");
+                    close(pipefd[0]);
+                    readEndClosed = true;
+                }
             }
             else if (events[i].data.fd == pipefd[1])
             {
                 printf("Selected fd = %d\n", events[i].data.fd);
                 write(pipefd[1], toPrint, sizeof(toPrint));
+                printf("written\n");
+                close(pipefd[1]);
+                writeEndClosed = true;
             }
         }
         sleep(1);
     }
-    close(pipefd[0]);
     return 0;
 }
